73450d0e9a7efc336d2cab0a8ed8001d2d48eb0a
imer <39924224+Imerpg@users.noreply.github.com>
2025-07-22 21:57:33 +0200

    Ajout de styles de mise en page au composant Profile

--- src/app/components/profile.tsx
+++ src/app/components/profile.tsx
@@ -1,6 +1,6 @@
 export default function Profile() {
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-600 text-white">
+    <div className="flex flex-col items-center justify-center bg-gray-600 text-white p-10 rounded-lg">
       <h1 className="text-3xl font-bold">Rémi Constans</h1>
     </div>
   );
