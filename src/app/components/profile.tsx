7cf56ff655db44462eac89e4e2ded9e8ebdf7652
imer <39924224+Imerpg@users.noreply.github.com>
2025-07-22 21:48:35 +0200

    Git init

---
+++ src/app/components/profile.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+  return (
+    <div className="flex flex-col items-center justify-center bg-gray-600 text-white">
+      <h1 className="text-3xl font-bold">Rémi Constans</h1>
+    </div>
+  );
+}
