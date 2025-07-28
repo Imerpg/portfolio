5919091498272cb0679182d7085f8f6c16378375
imer <39924224+Imerpg@users.noreply.github.com>
2025-07-28 21:23:55 +0200

    Add color and some style

--- src/app/components/profile.tsx
+++ src/app/components/profile.tsx
@@ -4,35 +4,40 @@ import Image from "next/image";
 
 export default function Profile() {
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-600 text-white p-10 rounded-lg">
-      <Image
-        src="/profile.jpg"
-        alt="Profile Picture"
-        width={150}
-        height={150}
-        className="mb-4 rounded-full object-cover w-40 h-40"
-      />
-      <h1 className="text-3xl font-bold">Rémi Constans</h1>
-      <div className="mt-6 flex gap-4">
-        <a href="https://github.com/Imerpg" className="flex items-center gap-2 text-lg hover:underline" target="_blank" rel="noopener noreferrer">
+    <div className="relative mx-auto max-w-md rounded-lg bg-gradient-to-tr from-fuchsia-300 to-blue-500 p-1 shadow-lg">
+      <div className="bg-slate-300/50 p-5 rounded-md flex flex-col justify-center-safe backdrop-blur-lg">
+        <div className="flex justify-center mb-4">
           <Image
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-            alt="GitHub"
-            width={24}
-            height={24}
+            src="/profile.jpg"
+            alt="Profile Picture"
+            width={150}
+            height={150}
+            className="mb-4 rounded-full object-cover w-40 h-40"
           />
-          GitHub
-        </a>
-        <a href="https://www.linkedin.com/in/remi-constans" className="flex items-center gap-2 text-lg hover:underline" target="_blank" rel="noopener noreferrer">
-          <Image
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
-            alt="LinkedIn"
-            width={24}
-            height={24}
-          />
-          LinkedIn
-        </a>
-      </div>
+        </div>
+
+        <h1 className="text-3xl font-bold">Rémi Constans</h1>
+        <div className="mt-6 flex gap-4">
+          <a href="https://github.com/Imerpg" className="flex items-center gap-2 text-lg hover:underline" target="_blank" rel="noopener noreferrer">
+            <Image
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+              alt="GitHub"
+              width={24}
+              height={24}
+            />
+            GitHub
+          </a>
+          <a href="https://www.linkedin.com/in/remi-constans" className="flex items-center gap-2 text-lg hover:underline" target="_blank" rel="noopener noreferrer">
+            <Image
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
+              alt="LinkedIn"
+              width={24}
+              height={24}
+            />
+            LinkedIn
+          </a>
+        </div>
+      </div>        
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const josefin_Sans = Josefin_Sans({
+  weight: "100",
+  subsets: ['latin'],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const josefin_Sans_bold = Josefin_Sans({
+  weight: "700",
+  subsets: ['latin'],
 });
+
 
 export const metadata: Metadata = {
   title: "Portfolio - Rémi Constans",
@@ -28,7 +29,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${josefin_Sans.className} antialiased`}
+        style={{userSelect: 'none'}}
       >
         {children}
       </body>
